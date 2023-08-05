7476ee9e972b721626dd518e3830230d7709cca9
lebrancconvas <lebrancconvas@gmail.com>
2023-08-05 23:47:32 +0700

    🔧 chore: update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://testautomationpractice.blogspot.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on-first-retry'
   },
 
   /* Configure projects for major browsers */
